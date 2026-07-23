0d14fa4af0b0f7abb0a3ca2e0008a4e2c1bfb7b4
RanjithKumar94 <ranjithkumar3593@gmail.com>
2026-07-23 18:31:51 +0530

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -1,1 +1,30 @@
 
+const canvas =
+document.getElementById("radar");
+
+
+const ctx =
+canvas.getContext("2d");
+
+
+
+ctx.fillStyle="#001100";
+
+ctx.fillRect(
+0,
+0,
+canvas.width,
+canvas.height
+);
+
+
+
+ctx.fillStyle="#00ff66";
+
+ctx.font="20px Arial";
+
+ctx.fillText(
+"SCC RADAR CONTROLLER",
+250,
+450
+);
