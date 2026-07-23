5ff442999f0e58f7064e9aba3d85f9895cb45ee9
RanjithKumar94 <ranjithkumar3593@gmail.com>
2026-07-23 21:02:15 +0530

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -1,29 +1,212 @@
-console.log("NEW CONTROLLER FILE RUNNING");
+// ======================================
+// SCC Radar Controller
+// ======================================
 
 
-function drawControllerRadar(){
-
-    console.log("RADAR LOOP RUNNING");
+console.log("Controller JS loaded");
 
 
-    ctx.fillStyle = "red";
 
-    ctx.beginPath();
+// Test aircraft
 
-    ctx.arc(
-        450,
-        300,
-        10,
-        0,
-        Math.PI * 2
-    );
+let controllerAircraft = [
 
-    ctx.fill();
+    {
+
+        callsign:"ETH773",
+
+        x:450,
+        y:300,
+
+        level:50,
+
+        assignedLevel:"",
+
+        heading:260,
+
+        speed:220
+
+    }
+
+];
 
 
-    requestAnimationFrame(drawControllerRadar);
+
+// ======================================
+// Draw Controller Aircraft
+// ======================================
+
+function drawControllerAircraft(){
+
+
+    controllerAircraft.forEach(ac=>{
+
+
+        // Aircraft blip
+
+        ctx.fillStyle="#00FF00";
+
+        ctx.beginPath();
+
+        ctx.arc(
+
+            ac.x,
+            ac.y,
+            5,
+            0,
+            Math.PI*2
+
+        );
+
+        ctx.fill();
+
+
+
+        // Label position
+
+        const labelX =
+        ac.x + 12;
+
+
+        const labelY =
+        ac.y - 15;
+
+
+
+        ctx.fillStyle="#00FF00";
+
+        ctx.font="14px Consolas";
+
+        ctx.textAlign="left";
+
+
+
+        // Callsign
+
+        ctx.fillText(
+
+            ac.callsign,
+
+            labelX,
+            labelY
+
+        );
+
+
+
+        // Level
+
+        let levelText;
+
+
+        if(ac.assignedLevel !== ""){
+
+
+            levelText =
+            "FL" +
+            ac.level +
+            "  " +
+            ac.assignedLevel;
+
+
+        }
+
+        else{
+
+
+            levelText =
+            "FL" +
+            ac.level;
+
+
+        }
+
+
+
+        ctx.fillText(
+
+            levelText,
+
+            labelX,
+            labelY + 16
+
+        );
+
+
+
+        // Heading and speed
+
+        ctx.fillText(
+
+            ac.heading +
+            "  " +
+            ac.speed,
+
+            labelX,
+            labelY + 32
+
+        );
+
+
+    });
+
 
 }
 
+
+
+
+// ======================================
+// Controller Radar Loop
+// ======================================
+
+function drawControllerRadar(){
+
+
+    ctx.clearRect(
+
+        0,
+        0,
+        canvas.width,
+        canvas.height
+
+    );
+
+
+
+    // Shared radar
+
+    drawBackground();
+
+    drawRoutes();
+
+    drawRunway();
+
+    drawTrafficCircuit();
+
+    drawCentreline();
+
+    drawCCB();
+
+
+
+    // Controller aircraft
+
+    drawControllerAircraft();
+
+
+
+    requestAnimationFrame(
+
+        drawControllerRadar
+
+    );
+
+
+}
+
+
+
+// Start
 
 drawControllerRadar();
